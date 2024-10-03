0e6d7fec54c000ab9ff59fe82a76618d37b72b8c
Mohcen Taher Chouireb <mouhcen.chouireb@ensia.edu.dz>
2024-10-03 15:02:11 +0100

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+import { nextui } from '@nextui-org/react'
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',     
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+		},
+		colors: {
+			"bg__main": "#F2F4F7", 
+			"custom__white": "#FCFCFC"
+		}
+	},
+	plugins: [],
+	darkMode: true,   
+	plugins: [nextui()]
+}
