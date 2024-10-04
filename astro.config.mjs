9b7557e55a6e0eff63d0a3ee20d545f7ec61f61d
Mohcen Taher Chouireb <mouhcen.chouireb@ensia.edu.dz>
2024-10-04 22:08:03 +0100

    added deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: netlify()
 });
